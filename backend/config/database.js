00211605f21bbaa340313ccb062df0bc143b12b2
GANASRI SENTHIL <108359413+ganasri-senthil@users.noreply.github.com>
2023-04-12 15:41:09 +0530

    Add files via upload

---
+++ backend/config/database.js
@@ -0,0 +1,13 @@
+const mongoose = require('mangoose');
+
+const connectDatabase = ()=>{
+    mongoose.connect(process.env.DB_LOCAL_URI,{
+        useNewUrlParser:true,
+        useUnifiedTopology:true
+    }).then(con=>{
+        console.log(`MongoDB is connected to the host: ${con.connection.host}`)
+    }).catch((err)=>{
+        console.log(err)
+    })
+}
+module.exports = connectDatabase;
